266957772a95ceb3c286473b416e2f5ddc9e5532
YayL <jonasbeast9@gmail.com>
2025-05-07 10:40:45 +0200

    Begun adding audio to engine

---
+++ src/engine/audio/audio.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <miniaudio.h>
+
+#include "engine/audio/device.h"
+#include "engine/audio/source.h"
+#include "engine/audio/sources.h"
+
+struct audio_context {
+    struct audio_device * device;
+    struct source_list sources;
+    ma_device_data_proc callback;
+    ma_context context;
+};
+
+struct audio_context * init_audio();
+
+void audio_set_device(struct audio_context * context, ma_device_info device);
+
+void audio_add_sound(struct audio_context * context, struct sound_source source);
